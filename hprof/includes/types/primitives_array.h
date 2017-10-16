e707fadba86c162e56a70e9a5e5560ae3658a303
pvoid <pvointervoid@gmail.com>
2017-10-16 18:41:51 +0200

    Do not access memory out of bounds during arrays iteration

--- hprof/includes/types/primitives_array.h
+++ hprof/includes/types/primitives_array.h
@@ -55,15 +55,23 @@ namespace hprof {
 
         class items_iterator {
         public:
-            items_iterator(const u_int8_t* data, jvm_type_t type, size_t id_size) : 
-                _offset(0), _data(data), _type(type), _item_size(jvm_type_t::size(type, id_size)), _current(_data, _offset, _item_size, _type) {
+            items_iterator(const u_int8_t* data, size_t data_size, jvm_type_t type, size_t id_size) : 
+                _offset(0), _data_size(data_size), _data(data), _type(type), _item_size(jvm_type_t::size(type, id_size)), _current(_data, _offset, _item_size, _type) {
             }
 
             bool operator!=(const items_iterator& src) const { return _data + _offset != src._data + src._offset; }
             bool operator==(const items_iterator& src) const { return _data + _offset == src._data + src._offset; }
     
             items_iterator& operator++() {
+                if (_offset == _data_size) {
+                    return *this;
+                }
+
                 _offset += _item_size;
+                if (_offset >= _data_size) {
+                    _current = array_item_impl_t { nullptr, 0, 0, jvm_type_t::JVM_TYPE_UNKNOWN };
+                    return *this;
+                }
                 _current = array_item_impl_t {_data, _offset, _item_size, _type};
                 return *this; 
             }
@@ -71,6 +79,7 @@ namespace hprof {
             const array_item_impl_t& operator*() const { return _current; }
                 const array_item_impl_t* operator->() const { return &_current; }
         private:
+            size_t _data_size;
             size_t _offset;
             mutable const u_int8_t* _data;
             jvm_type_t _type;
@@ -90,12 +99,12 @@ namespace hprof {
         virtual size_t length() const override { return _length; }
         virtual jvm_type_t item_type() const override { return _type; }
 
-        virtual iterator begin() const override { return items_iterator {_data, _type, id_size()}; }
-        virtual iterator end() const override { return items_iterator {_data + jvm_type_t::size(_type, id_size()) * _length, _type, id_size()}; }
+        virtual iterator begin() const override { return items_iterator {_data, _data_size, _type, id_size()}; }
+        virtual iterator end() const override { return items_iterator {_data + jvm_type_t::size(_type, id_size()) * _length, 0, _type, id_size()}; }
         virtual iterator operator[](size_t index) const override {
             if (index < _length) {
                 auto offset = jvm_type_t::size(_type, id_size()) * index;
-                return items_iterator { _data + offset, _type, id_size() };
+                return items_iterator { _data + offset, _data_size - offset, _type, id_size() };
             }
             return end();
         }
@@ -104,15 +113,16 @@ namespace hprof {
     public:
         static primitives_array_info_impl_ptr_t create(u_int8_t id_size, jvm_id_t id, jvm_type_t type, size_t length, size_t data_size) {
             auto mem = new (std::nothrow) u_int8_t[sizeof(primitives_array_info_impl_t) + data_size];
-            return primitives_array_info_impl_ptr_t { new (mem) primitives_array_info_impl_t(id_size, id, type, length) };
+            return primitives_array_info_impl_ptr_t { new (mem) primitives_array_info_impl_t(id_size, id, type, length, data_size) };
         }
     private:
-        primitives_array_info_impl_t(u_int8_t id_size, jvm_id_t id, jvm_type_t type, size_t length) : 
-        object_info_impl_t(id_size, id), _data(reinterpret_cast<u_int8_t*>(this) + sizeof(primitives_array_info_impl_t)), 
+        primitives_array_info_impl_t(u_int8_t id_size, jvm_id_t id, jvm_type_t type, size_t length, size_t data_size) : 
+        object_info_impl_t(id_size, id), _data_size(data_size), _data(reinterpret_cast<u_int8_t*>(this) + sizeof(primitives_array_info_impl_t)), 
         _length(length), _type(type) {}
     
     private:
         u_int8_t* _data;
+        size_t _data_size;
         size_t _length;
         jvm_type_t _type;
     };
